27b38b2d0d686fbf76ff71b8d0cd6f89704a15b3
Daniyar Nasirov <dannasirov312@gmail.com>
2025-01-16 19:38:02 +0600

    created types file, refactor css files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [] = useState(0)
-
   return (
-    <>
-    </>
-  )
+      <div className="App">
+        <header className="App-header">
+          <h1>Product Store</h1>
+        </header>
+        <main>
+        </main>
+      </div>
+  );
 }
 
-export default App
+export default App;
